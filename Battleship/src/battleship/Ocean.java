7443a009e41fbfb56c23c385b9ae8a1ca71e0cc7
rbennett485 <rbennett485@gmail.com>
2014-04-07 11:27:29 +0100

    Method stubs added

---
+++ Battleship/src/battleship/Ocean.java
@@ -0,0 +1,45 @@
+package battleship;
+
+public class Ocean {
+	
+	public Ocean() {
+		
+	}
+
+	public void placeAllShipsRandomly() {
+		
+	}
+
+	public boolean isOccupied(int row, int column) {
+		return false;
+	}
+	
+	public boolean shootAt(int row, int column) {
+		return false;
+	}
+	
+	public int getShotsFired() {
+		return 0;
+	}
+	
+	public int getHitCount() {
+		return 0;
+	}
+	
+	public int getShipsSunk() {
+		return 0;
+	}
+	
+	public boolean isGameOver() {
+		return false;
+	}
+	
+	public Ship[][] getShipArray() {
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+	}
+}

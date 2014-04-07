21f3b1daed462888550ab65fc8461b48162d71a2
rbennett485 <rbennett485@gmail.com>
2014-04-07 11:16:20 +0100

    single package

---
+++ Battleship/src/battleship/BattleshipGameTest.java
@@ -0,0 +1,14 @@
+package battleship;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BattleshipGameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

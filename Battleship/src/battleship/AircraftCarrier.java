b0bb476ca27f97766409e22c063d8e170ac756ec
rbennett485 <rbennett485@gmail.com>
2014-04-07 12:25:29 +0100

    method stubs created

---
+++ Battleship/src/battleship/AircraftCarrier.java
@@ -0,0 +1,17 @@
+package battleship;
+
+public class AircraftCarrier extends Ship {
+
+	@Override
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getShipType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

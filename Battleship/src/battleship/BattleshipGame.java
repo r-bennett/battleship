1a3445224f9bf9683a9cf6582aaf134ba1c3ca86
rbennett485 <rbennett485@gmail.com>
2014-04-07 11:43:49 +0100

    gameOver delegated to Ocean

--- Battleship/src/battleship/BattleshipGame.java
+++ Battleship/src/battleship/BattleshipGame.java
@@ -3,18 +3,13 @@ package battleship;
 public class BattleshipGame {
 	
 	private static Ocean ocean;
-	private static boolean gameOver;
-	
-	static {
-		gameOver = false;
-	}
 
 	public static void main(String[] args) {
 		ocean = new Ocean();
 		ocean.placeAllShipsRandomly();
 		System.out.println(ocean.toString());
 		
-		while(!gameOver) {
+		while(!ocean.isGameOver()) {
 			// do stuff
 		}
 	}

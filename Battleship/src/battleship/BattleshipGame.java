d99cb83643197c8531c6632b84d328f36b8fb17c
rbennett485 <rbennett485@gmail.com>
2014-04-07 11:38:49 +0100

    Structure of main method added

--- Battleship/src/battleship/BattleshipGame.java
+++ Battleship/src/battleship/BattleshipGame.java
@@ -1,10 +1,22 @@
 package battleship;
 
 public class BattleshipGame {
+	
+	private static Ocean ocean;
+	private static boolean gameOver;
+	
+	static {
+		gameOver = false;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ocean = new Ocean();
+		ocean.placeAllShipsRandomly();
+		System.out.println(ocean.toString());
+		
+		while(!gameOver) {
+			// do stuff
+		}
 	}
 
 }

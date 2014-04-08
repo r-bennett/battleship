35ead5cbabc81f9a37bb65dc024e0ed5276c88d3
rbennett485 <rbennett485@gmail.com>
2014-04-08 18:48:33 +0100

    added basic user input functionality

--- Battleship/src/battleship/BattleshipGame.java
+++ Battleship/src/battleship/BattleshipGame.java
@@ -1,17 +1,25 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class BattleshipGame {
 	
 	private static Ocean ocean;
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		ocean = new Ocean();
 		ocean.placeAllShipsRandomly();
 		System.out.println(ocean.toString());
 		
 		while(!ocean.isGameOver()) {
-			// do stuff
+			int column = sc.nextInt();
+			int row = sc.nextInt();
+			ocean.shootAt(row, column);
+			System.out.println();
+			System.out.println(ocean.toString());
 		}
+		sc.close();
 	}
 
 }

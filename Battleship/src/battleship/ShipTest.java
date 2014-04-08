cdf2e9cf273480a01074ccbb0c8e096b977485ba
rbennett485 <rbennett485@gmail.com>
2014-04-08 20:20:50 +0100

    passes getLenth() test

--- Battleship/src/battleship/ShipTest.java
+++ Battleship/src/battleship/ShipTest.java
@@ -2,13 +2,37 @@ package battleship;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ShipTest {
+	public Ship ac;
+	public Ship bs;
+	public Ship d;
+	public Ship es;
+	public Ship pb;
+	public Ship s;
+	
+	@Before
+	public void buildUp() {
+		ac = new AircraftCarrier();
+		bs = new Battleship();
+		d = new Destroyer();
+		es = new EmptySea();
+		pb = new PatrolBoat();
+		s = new Submarine();				
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getLengthTest() {
+		assertEquals(ac.getLength(),5);
+		assertEquals(bs.getLength(),4);
+		assertEquals(d.getLength(),2);
+		assertEquals(es.getLength(),1);
+		assertEquals(pb.getLength(),1);
+		assertEquals(s.getLength(),3);
 	}
+	
+	
 
 }

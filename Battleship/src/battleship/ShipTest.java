bb6b1c3c1f54e43b781791df9b6e84f96542197e
rbennett485 <rbennett485@gmail.com>
2014-04-08 20:15:47 +0100

    fails

---
+++ Battleship/src/battleship/ShipTest.java
@@ -0,0 +1,14 @@
+package battleship;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShipTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

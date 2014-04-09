8f26604115ddff2e6dfcf62e49089e376a965c4e
rbennett485 <rbennett485@gmail.com>
2014-04-09 21:34:04 +0100

    added print score and play again options

--- Battleship/src/battleship/BattleshipGame.java
+++ Battleship/src/battleship/BattleshipGame.java
@@ -3,21 +3,32 @@ package battleship;
 import java.util.Scanner;
 
 public class BattleshipGame {
-	
+
 	private static Ocean ocean;
+	private static boolean playAgain = true;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ocean = new Ocean();
-		ocean.placeAllShipsRandomly();
-		System.out.println(ocean.toString());
-		
-		while(!ocean.isGameOver()) {
-			int column = sc.nextInt();
-			int row = sc.nextInt();
-			ocean.shootAt(row, column);
-			System.out.println();
+		while(playAgain) {
+			ocean = new Ocean();
+			ocean.placeAllShipsRandomly();
 			System.out.println(ocean.toString());
+
+			while(!ocean.isGameOver()) {
+				int column = sc.nextInt();
+				int row = sc.nextInt();
+				ocean.shootAt(row, column);
+				System.out.println();
+				System.out.println(ocean.toString());
+			}
+
+			System.out.println();
+			System.out.println("GAME OVER");
+			System.out.println("Shots taken: " + ocean.getShotsFired());
+			System.out.println();
+			System.out.println("Play again? (y/n): ");
+			char choice = sc.next().toLowerCase().charAt(0);
+			playAgain = choice=='y';
 		}
 		sc.close();
 	}
